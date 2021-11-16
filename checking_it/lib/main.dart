226ed7083cec83816540675a1975477de2ac39c4
MiNHAZ-33 <miminhaz33@gmail.com>
2021-11-16 18:03:23 +0600

    just checing the most basic concepts

---
+++ checking_it/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'After Class',
+      home: HomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is the app bar'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+    );
+  }
+}
