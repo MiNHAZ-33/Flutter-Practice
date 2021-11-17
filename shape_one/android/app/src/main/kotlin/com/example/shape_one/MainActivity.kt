e53f0b1b76085269e5359b41d3af8896ff2a5861
MiNHAZ-33 <miminhaz33@gmail.com>
2021-11-17 21:14:03 +0600

    working on the first shape

---
+++ shape_one/android/app/src/main/kotlin/com/example/shape_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shape_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ shape_one/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shape One',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Choose the cards from below',
+          style: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.white60,
+      ),
+      body: CardMaker(),
+    );
+  }
+}
+
+class CardMaker extends StatelessWidget {
+  CardMaker({Key? key}) : super(key: key);
+
+  // final List<AllData> allData = [
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  //   AllData(ic: Icon(Icons.home), lab: 'Hello', am: 888),
+  // ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 6,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (context, int index) {
+        return Container(
+          children: [
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+            cardMaker(icon: Icons.ac_unit, title: 'Home', amount: 79),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Widget cardMaker(
+    {required IconData icon, required String title, required double amount}) {
+  return Card(
+      elevation: 20,
+      color: Colors.pinkAccent,
+      shadowColor: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      margin: EdgeInsets.all(10),
+      child: Container(
+        padding: EdgeInsets.only(left: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
+              size: 40,
+            ),
+            Text(title, style: TextStyle(color: Colors.white, fontSize: 24)),
+            Text(
+              amount.toString(),
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ],
+        ),
+      ));
+}
+
+// class AllData {
+//   Icon ic;
+//   String lab;
+//   double am;
+
+//   AllData({
+//     required this.ic,
+//     required this.lab,
+//     required this.am,
+//   });
+// }
