e3692e064fe3c24a2a4fc21a6ffb8444c0411b26
MiNHAZ-33 <miminhaz33@gmail.com>
2021-11-26 23:22:58 +0600

    Routes and Stacks are added

--- homescreen/lib/homepage.dart
+++ homescreen/lib/homepage.dart
@@ -6,9 +6,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueAccent,
       appBar: AppBar(
         title: Text('Welcome to the homescreen'),
         centerTitle: true,
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amberAccent,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Click me to go back to log in page'),
+            ),
+          ],
+        ),
       ),
     );
   }
