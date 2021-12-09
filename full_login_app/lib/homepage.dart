5c3d5fbbeff740f61fd52c5529d52b8879b2167a
MiNHAZ-33 <miminhaz33@gmail.com>
2021-12-09 23:52:21 +0600

    Working on app bar customization

--- full_login_app/lib/homepage.dart
+++ full_login_app/lib/homepage.dart
@@ -10,6 +10,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black12,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Column(
+          children: [Text('Message from $name'), Icon(Icons.home)],
+        ),
+        backgroundColor: Colors.pink,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: SizedBox(),
+        ),
+      ),
       body: Column(
         children: [
           Text(name),
